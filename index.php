41030be630a483782e3e181d1ac31240bf306a50
Damien <boenapfel.damien@yahoo.fr>
2026-02-26 17:17:17 +0100

    E-commerce.docker

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    $shoes = [
+        ['title' => 'shoes1', 'description' => 'Élégantes et polyvalentes, parfaites pour un look casual ou urbain.', 'image' => './assets/images/produit1.jpg'],
+        ['title' => 'shoes2', 'description' => 'Apportez du style et du confort à chaque pas avec leur design audacieux.', 'image' => './assets/images/produit2.jpg'],
+        ['title' => 'shoes3', 'description' => 'Optimisées pour la performance et le confort, idéales pour vos entraînements quotidiens.', 'image' => './assets/images/produit3.jpg'],
+        ['title' => 'shoes4', 'description' => 'Sophistiquées et durables, pour allier luxe et confort au quotidien.', 'image' => './assets/images/produit4.jpg'],
+     ];
+
+    include('./template/index.phtml');
+?>
